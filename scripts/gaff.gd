453d929b1ab40f4f836c7c41660be24d3306ed3c
Somebud0180 <ethanlagera@icloud.com>
2025-12-20 20:58:15 +0800

    Implemented camera and animated sprite functionality to character, added character to main game scene along with placeholder tiles

--- scripts/gaff.gd
+++ scripts/gaff.gd
@@ -9,11 +9,11 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -21,5 +21,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = true
+	elif velocity.x == 0:
+		$AnimatedSprite2D.play("default")
+	
 	move_and_slide()
