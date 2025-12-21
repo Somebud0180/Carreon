1c5602bc1888557211bcafc94de10765dc644c4b
Somebud0180 <ethanlagera@icloud.com>
2025-12-21 22:34:21 +0800

    Added sprinting and keymaps to player controls, adjusted documentation and code within player code

--- scripts/gaff.gd
+++ scripts/gaff.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
+const SPRINT_SPEED = 250.0
 const JUMP_VELOCITY = -400.0
+const ACCELERATION = 250
+const FRICTION = 550     # How fast player stops
+const AIR_FRICTION = 100 # How fast player stops in air
+const turn_speed = 400   # How fast player snaps to the opposite direction
 
+var is_sprinting = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,17 +17,32 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = max(min(velocity.x + (direction * delta * (SPEED/1.5)), SPEED), -SPEED)
+	# Handle sprint.
+	if Input.is_action_pressed("sprint"):
+		is_sprinting = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * delta * 2)
+		is_sprinting = false
+		
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		# Check if moving opposite to current velocity
+		var is_turning = sign(direction) != sign(velocity.x) and velocity.x != 0
+		
+		var current_accel = turn_speed if is_turning else ACCELERATION
+		var current_speed = SPRINT_SPEED if is_sprinting else SPEED
+		
+		velocity.x = move_toward(velocity.x, direction * current_speed, current_accel * delta)
+	else:
+		var current_friction = AIR_FRICTION if !is_on_floor() else FRICTION
+		velocity.x = move_toward(velocity.x, 0, current_friction * delta)
 	
+	move_and_slide()
+
+func _process(_delta: float) -> void:
 	if velocity.x > 0:
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.flip_h = false
@@ -30,5 +51,3 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = true
 	elif velocity.x == 0:
 		$AnimatedSprite2D.play("default")
-	
-	move_and_slide()
