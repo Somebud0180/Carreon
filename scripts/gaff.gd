718e83170888b8c6603ab8064299e4cad3ff4275
Somebud0180 <ethanlagera@icloud.com>
2025-12-21 17:04:36 +0800

    setup the new tileset, adjusted player control

--- scripts/gaff.gd
+++ scripts/gaff.gd
@@ -18,9 +18,9 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = max(min(velocity.x + (direction * delta * (SPEED/1.5)), SPEED), -SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta * 2)
 	
 	if velocity.x > 0:
 		$AnimatedSprite2D.play("walk")
