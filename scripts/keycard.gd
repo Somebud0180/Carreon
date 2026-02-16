56280d442e456da5ea35018ca1eec18b64bd7678
Somebud0180 <ethanlagera@icloud.com>
2026-02-17 02:32:30 +0800

    Completed keycard functionality

--- scripts/keycard.gd
+++ scripts/keycard.gd
@@ -4,14 +4,24 @@ extends Node2D
 @export var keycard_id: int = 0 # An identifier that defines what numbered door this keycard unlocks
 
 var is_collected: bool = false
-
+var _actor: Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_2d.connect("body_entered", _on_body_entered)
+	area_2d.connect("body_exited", _on_body_exited)
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player and !is_collected:
-		if body.add_item("key", keycard_id):
+	if body is Player:
+		_actor = body
+		body.interactable = self
+
+func _on_body_exited(body: Node2D) -> void:
+	if body == _actor:
+		body.interactable = null
+		_actor = null
+
+func interact() -> void:
+	if _actor.add_item("key", keycard_id):
 			is_collected = true
 			visible = false
